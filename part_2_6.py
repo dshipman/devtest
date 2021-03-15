6d89aebbcc52475b51bf7cfb3fa4c1657a523749
David Shipman <dshipman@gmail.com>
2021-03-15 12:37:09 +1100

    2.6

--- part_2_6.py
+++ part_2_6.py
@@ -5,12 +5,17 @@ so that other people reading this code can quickly understand what this function
 You may also rename the functions if you can think of clearer names.
 """
 
-def step_function_maker(start_time, end_time, value):
-    def my_function(time):
+
+def create_step_function(start_time, end_time, value):
+    """
+    Create a step function that takes a single input ('time'), and returns <value> if that input is 
+    between start_time and end_time (inclusive), otherwise 0.0
+    """
+    def step_function(time):
         if start_time <= time <= end_time:
             y = value
         else:
             y = 0.0
         return y
 
-    return my_function
+    return step_function
