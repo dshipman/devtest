5be8e66a9cd86ab8f07a5ebd0bddc25fad587d34
David Shipman <dshipman@gmail.com>
2021-03-15 13:54:21 +1100

    4.3

--- part_4_3.py
+++ part_4_3.py
@@ -22,6 +22,20 @@ def get_noisy_values():
 
 def test_get_noisy_values():
     values = get_noisy_values()
-    expected = np.array([1, 2, 3, 4, 5])
+    # expected = np.array([1, 2, 3, 4, 5])
     # This will fail most of the time.
-    assert (values == expected).all()
+    # assert (values == expected).all()
+	
+	signal = np.array([1, 2, 3, 4, 5])
+
+	assert (values >= signal).all()
+	assert (values < signal + 1.0).all()
+
+
+	values2 = get_noisy_values()
+
+	# Check that randomizer is being random...
+	assert(values.sum != values2.sum())
+	# Actually kind of a bad test, since it could conceivably fail,
+	# but this is vanishingly unlikely, and such a condition could be 
+	# regarded as a bug in its own right...
